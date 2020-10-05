e2ce1ecf220c3521696e1a3325d7d19316bf96a0
David Zhang <dzz0615@gmail.com>
2020-10-06 01:06:52 +0700

    added babel compile opts, removed window debug option (for now)

--- src/components/VideoProvider/useRoom/useRoom.tsx
+++ src/components/VideoProvider/useRoom/useRoom.tsx
@@ -5,9 +5,13 @@ import Video, { ConnectOptions, LocalTrack, Room } from 'twilio-video';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
 // @ts-ignore
-window.TwilioVideo = Video; // for debugging purposes
+// window.TwilioVideo = Video; // for debugging purposes
 
-export default function useRoom(localTracks: LocalTrack[], onError: Callback, options?: ConnectOptions) {
+export default function useRoom(
+  localTracks: LocalTrack[],
+  onError: Callback,
+  options?: ConnectOptions,
+) {
   const [room, setRoom] = useState<Room>(new EventEmitter() as Room);
   const [isConnecting, setIsConnecting] = useState(false);
   const localTracksRef = useRef<LocalTrack[]>([]);
@@ -53,7 +57,9 @@ export default function useRoom(localTracks: LocalTrack[], onError: Callback, op
             // All video tracks are published with 'low' priority. This works because the video
             // track that is displayed in the 'MainParticipant' component will have it's priority
             // set to 'high' via track.setPriority()
-            newRoom.localParticipant.publishTrack(track, { priority: track.kind === 'video' ? 'low' : 'standard' })
+            newRoom.localParticipant.publishTrack(track, {
+              priority: track.kind === 'video' ? 'low' : 'standard',
+            }),
           );
 
           setIsConnecting(false);
@@ -69,10 +75,10 @@ export default function useRoom(localTracks: LocalTrack[], onError: Callback, op
         (error) => {
           onError(error);
           setIsConnecting(false);
-        }
+        },
       );
     },
-    [onError]
+    [onError],
   );
 
   return { room, isConnecting, connect };
