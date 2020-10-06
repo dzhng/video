8d10715c70abb8b1246091f808cc863ed95e97ad
David Zhang <dzz0615@gmail.com>
2020-10-06 23:12:08 +0700

    better auth error printout

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useAppState } from '../../state';
 
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -9,6 +10,7 @@ import GoogleLogo from './google-logo.svg';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 import { useLocation, useHistory } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   container: {
@@ -75,8 +77,15 @@ export default function LoginPage() {
   return (
     <ThemeProvider theme={theme}>
       <Grid container justify="center" alignItems="flex-start" className={classes.container}>
-        {authError}
         <Paper className={classes.paper} elevation={6}>
+          <div>
+            {authError && (
+              <Typography variant="caption" className={classes.errorMessage}>
+                <ErrorOutlineIcon />
+                {authError.message}
+              </Typography>
+            )}
+          </div>
           <Button
             variant="contained"
             className={classes.button}
