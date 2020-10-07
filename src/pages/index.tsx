4c2a91607ba1bf9e66e5a7c9fc0596f4bf96e594
David Zhang <dzz0615@gmail.com>
2020-10-07 21:05:12 +0700

    fixed compile issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from '~/theme';
 
 // disable SSR on the video component itself, since it uses browser features
-const App = dynamic(() => import('~/App'), { ssr: false });
+const App = dynamic(() => import('~/components/Layout'), { ssr: false });
 
 function IndexPage() {
   return (
