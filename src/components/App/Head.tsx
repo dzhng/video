18b5387c1200d8e2d4ac77e916bccaacafb93c04
David Zhang <dzz0615@gmail.com>
2020-10-08 23:12:01 +0700

    fixed head

---
+++ src/components/App/Head.tsx
@@ -0,0 +1,43 @@
+import NextHead from 'next/head';
+
+const defaultTitle = '';
+const defaultDescription = '';
+const defaultOGURL = '';
+const defaultOGImage = '';
+
+interface HeadProps {
+  title: string;
+  description: string;
+  url: string;
+  ogImage: string;
+}
+
+export default function Head({ title, description, url, ogImage }: HeadProps) {
+  return (
+    <NextHead>
+      <meta charSet="UTF-8" />
+      <title>{title ?? defaultTitle}</title>
+      <meta name="description" content={description ?? defaultDescription} />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:url" content={url ?? defaultOGURL} />
+      <meta property="og:title" content={title ?? defaultTitle} />
+      <meta property="og:description" content={description ?? defaultDescription} />
+      <meta name="twitter:site" content={url ?? defaultOGURL} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={ogImage ?? defaultOGImage} />
+      <meta property="og:image" content={ogImage ?? defaultOGImage} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+    </NextHead>
+  );
+}
