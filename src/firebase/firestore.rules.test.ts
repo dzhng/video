6bbf2ede306f5da2bdd47b7691ac64457b75face
David Zhang <dzz0615@gmail.com>
2020-11-22 23:49:40 +0700

    call rules and tests

--- src/firebase/firestore.rules.test.ts
+++ src/firebase/firestore.rules.test.ts
@@ -73,123 +73,165 @@ describe('firebase cloud firestore database rules', () => {
     });
   });
 
-  it('allows anyone to read network data', async () => {
-    const db = getAuthedFirestore(null);
-    const network = db.collection('networks').doc('network');
-    const doc = await firebase.assertSucceeds(network.get());
-    expect(doc.data().name).toEqual('A Network');
+  describe('network', () => {
+    it('allows anyone to read network data', async () => {
+      const db = getAuthedFirestore(null);
+      const network = db.collection('networks').doc('network');
+      const doc = await firebase.assertSucceeds(network.get());
+      expect(doc.data().name).toEqual('A Network');
+    });
+
+    it('require users to log in and be owner before updating network', async () => {
+      const db = getAuthedFirestore(null);
+      const network = db.collection('networks').doc('network');
+      await firebase.assertFails(network.set({ name: 'hello world' }));
+    });
+
+    it('does not allow anyone to create a new network', async () => {
+      const db = getAuthedFirestore({ uid: 'OwnerUser' });
+      const network = db.collection('networks').doc('test');
+      await firebase.assertFails(network.set({ name: 'hello world' }));
+    });
+
+    it('allows logged in owners to update network', async () => {
+      const db = getAuthedFirestore({ uid: 'OwnerUser' });
+      const network = db.collection('networks').doc('network');
+      await firebase.assertSucceeds(network.update({ name: 'hello world' }));
+    });
+
+    it('does not allow random data to be added to network', async () => {
+      const db = getAuthedFirestore({ uid: 'OwnerUser' });
+      const network = db.collection('networks').doc('network');
+      await firebase.assertFails(network.update({ name: 'hello world', random: 'data' }));
+    });
   });
 
-  it('require users to log in and be owner before updating network', async () => {
-    const db = getAuthedFirestore(null);
-    const network = db.collection('networks').doc('network');
-    await firebase.assertFails(network.set({ name: 'hello world' }));
+  describe('user', () => {
+    it('only allow user to update their own profile', async () => {
+      const db = getAuthedFirestore({ uid: 'alice' });
+      const charlie = db.collection('users').doc('charlie');
+      await firebase.assertFails(
+        charlie.update({
+          bio: 'hello world',
+        }),
+      );
+
+      const alice = db.collection('users').doc('alice');
+      await firebase.assertSucceeds(
+        alice.update({
+          bio: 'hello world',
+        }),
+      );
+    });
+
+    it('should enforce user profiles fields', async () => {
+      const db = getAuthedFirestore({ uid: 'alice' });
+      const profile = db.collection('users').doc('alice');
+      await firebase.assertFails(
+        profile.update({
+          birthday: 'January 1',
+        }),
+      );
+      await firebase.assertSucceeds(
+        profile.update({
+          bio: 'hello world',
+        }),
+      );
+    });
+
+    it('should not let users create profile', async () => {
+      const db = getAuthedFirestore({ uid: 'alice' });
+      await firebase.assertFails(
+        db.collection('users').doc('bob').set({
+          bio: 'hello world',
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        }),
+      );
+    });
+
+    it('should let anyone read any profile', async () => {
+      const db = getAuthedFirestore(null);
+      const profile = db.collection('users').doc('alice');
+      await firebase.assertSucceeds(profile.get());
+    });
   });
 
-  it('does not allow anyone to create a new network', async () => {
-    const db = getAuthedFirestore({ uid: 'OwnerUser' });
-    const network = db.collection('networks').doc('test');
-    await firebase.assertFails(network.set({ name: 'hello world' }));
-  });
+  describe('call', () => {
+    it('should let anyone create a call', async () => {
+      const db = getAuthedFirestore({ uid: 'alice' });
+      const call = db.collection('networks').doc('network').collection('calls').doc('call');
+      await firebase.assertSucceeds(
+        call.set({
+          name: 'Test Room',
+          participants: ['alice', 'charlie'],
+          externalEmails: ['hello@world.com'],
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        }),
+      );
+    });
 
-  it('allows logged in owners to update network', async () => {
-    const db = getAuthedFirestore({ uid: 'OwnerUser' });
-    const network = db.collection('networks').doc('network');
-    await firebase.assertSucceeds(network.update({ name: 'hello world' }));
-  });
+    it('should let anyone access a call', async () => {
+      const db = getAuthedFirestore(null);
+      const call = db.collection('networks').doc('network').collection('calls').doc('call');
+      await firebase.assertSucceeds(call.get());
+    });
 
-  it('does not allow random data to be added to network', async () => {
-    const db = getAuthedFirestore({ uid: 'OwnerUser' });
-    const network = db.collection('networks').doc('network');
-    await firebase.assertFails(network.update({ name: 'hello world', random: 'data' }));
-  });
+    it('should force the creator of the call to be a participant', async () => {
+      const db = getAuthedFirestore({ uid: 'alice' });
+      const call = db.collection('networks').doc('network').collection('calls').doc('call');
+      await firebase.assertFails(
+        call.set({
+          name: 'Test Room',
+          participants: ['charlie'],
+          externalEmails: ['hello@world.com'],
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        }),
+      );
 
-  it('only allow user to update their own profile', async () => {
-    const db = getAuthedFirestore({ uid: 'alice' });
-    const charlie = db.collection('users').doc('charlie');
-    await firebase.assertFails(
-      charlie.update({
-        bio: 'hello world',
-      }),
-    );
+      await firebase.assertFails(
+        call.set({
+          name: 'Test Room',
+          participants: [],
+          externalEmails: ['hello@world.com'],
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        }),
+      );
+    });
 
-    const alice = db.collection('users').doc('alice');
-    await firebase.assertSucceeds(
-      alice.update({
-        bio: 'hello world',
-      }),
-    );
-  });
-
-  it('should enforce user profiles fields', async () => {
-    const db = getAuthedFirestore({ uid: 'alice' });
-    const profile = db.collection('users').doc('alice');
-    await firebase.assertFails(
-      profile.update({
-        birthday: 'January 1',
-      }),
-    );
-    await firebase.assertSucceeds(
-      profile.update({
-        bio: 'hello world',
-      }),
-    );
-  });
-
-  it('should not let users create profile', async () => {
-    const db = getAuthedFirestore({ uid: 'alice' });
-    await firebase.assertFails(
-      db.collection('users').doc('bob').set({
-        bio: 'hello world',
+    it('should only let call participants update the call', async () => {
+      const alice = getAuthedFirestore({ uid: 'alice' });
+      let call = alice.collection('networks').doc('network').collection('calls').doc('call');
+      call.set({
+        name: 'Test Room',
+        participants: ['alice', 'charlie'],
+        externalEmails: ['hello@world.com'],
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      }),
-    );
+      });
+
+      const bob = getAuthedFirestore({ uid: 'bob' });
+      call = bob.collection('networks').doc('network').collection('calls').doc('call');
+      await firebase.assertFails(
+        call.update({
+          name: 'Test Room 2',
+        }),
+      );
+    });
+
+    it('should let existing participants remove themselves from the call', async () => {
+      const db = getAuthedFirestore({ uid: 'alice' });
+      let call = db.collection('networks').doc('network').collection('calls').doc('call');
+      call.set({
+        name: 'Test Room',
+        participants: ['alice', 'charlie'],
+        externalEmails: ['hello@world.com'],
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+
+      await firebase.assertSucceeds(
+        call.update({
+          participants: firebase.firestore.FieldValue.arrayRemove('alice'),
+        }),
+      );
+    });
   });
-
-  it('should let anyone read any profile', async () => {
-    const db = getAuthedFirestore(null);
-    const profile = db.collection('users').doc('alice');
-    await firebase.assertSucceeds(profile.get());
-  });
-
-  /*it('should let anyone create a room', async () => {
-    const db = getAuthedFirestore({ uid: 'alice' });
-    const room = db.collection('rooms').doc('firebase');
-    await firebase.assertSucceeds(
-      room.set({
-        owner: 'alice',
-        topic: 'All Things Firebase',
-      }),
-    );
-  });
-
-  it('should force people to name themselves as room owner when creating a room', async () => {
-    const db = getAuthedFirestore({ uid: 'alice' });
-    const room = db.collection('rooms').doc('firebase');
-    await firebase.assertFails(
-      room.set({
-        owner: 'scott',
-        topic: 'Firebase Rocks!',
-      }),
-    );
-  });
-
-  it('should not let one user steal a room from another user', async () => {
-    const alice = getAuthedFirestore({ uid: 'alice' });
-    const bob = getAuthedFirestore({ uid: 'bob' });
-
-    await firebase.assertSucceeds(
-      bob.collection('rooms').doc('snow').set({
-        owner: 'bob',
-        topic: 'All Things Snowboarding',
-      }),
-    );
-
-    await firebase.assertFails(
-      alice.collection('rooms').doc('snow').set({
-        owner: 'alice',
-        topic: 'skiing > snowboarding',
-      }),
-    );
-  });*/
 });
