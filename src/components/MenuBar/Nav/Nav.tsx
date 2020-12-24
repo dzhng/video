e13108fedeed9226585c6eeb69041198ace1af44
David Zhang <dzz0615@gmail.com>
2020-12-24 20:55:23 +0700

    moved nav to seperate file

---
+++ src/components/MenuBar/Nav/Nav.tsx
@@ -0,0 +1,210 @@
+import React, { useCallback, useState } from 'react';
+import Link from 'next/link';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { VideoCall as VideoCallIcon, PresentToAll as PresentIcon } from '@material-ui/icons';
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Hidden,
+  Drawer,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import { isBrowser } from '~/utils';
+import { useAppState } from '~/state';
+
+const NewWorkspaceValue = '__New_Workspace__';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: theme.sidebarWidth,
+        flexShrink: 0,
+      },
+    },
+    select: {
+      width: theme.sidebarWidth - theme.spacing(2) * 2,
+      margin: theme.spacing(2),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    // to create padding for content
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: theme.sidebarWidth,
+    },
+  }),
+);
+
+export default function Nav({
+  mobileOpen,
+  toggleOpen,
+}: {
+  mobileOpen: boolean;
+  toggleOpen(): void;
+}) {
+  const classes = useStyles();
+  const [isCreatingWorkspace, setIsCreatingWorkspace] = useState(false);
+  const [newWorkspaceName, setNewWorkspaceName] = useState('');
+  const {
+    workspaces,
+    isWorkspacesReady,
+    currentWorkspaceId,
+    setCurrentWorkspaceId,
+    createWorkspace,
+  } = useAppState();
+
+  const handleWorkspaceChange = useCallback(
+    (event: React.ChangeEvent<{ value: unknown }>) => {
+      const value = event.target.value as string;
+      if (value === NewWorkspaceValue) {
+        setIsCreatingWorkspace(true);
+      } else {
+        setCurrentWorkspaceId(value);
+      }
+    },
+    [setCurrentWorkspaceId],
+  );
+
+  const handleCreateWorkspace = useCallback(() => {
+    setIsCreatingWorkspace(false);
+    createWorkspace(newWorkspaceName);
+  }, [newWorkspaceName, createWorkspace]);
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+
+      {isWorkspacesReady && workspaces ? (
+        <FormControl variant="outlined" className={classes.select}>
+          <InputLabel>Workspace</InputLabel>
+          <Select
+            label="Workspace"
+            value={currentWorkspaceId ?? ''}
+            onChange={handleWorkspaceChange}
+          >
+            {workspaces.map((workspace) => (
+              <MenuItem key={workspace.id} value={workspace.id}>
+                <Typography variant="h4">{workspace.name}</Typography>
+              </MenuItem>
+            ))}
+            <MenuItem value={NewWorkspaceValue}>
+              <Typography variant="h4">New Workspace</Typography>
+            </MenuItem>
+          </Select>
+        </FormControl>
+      ) : (
+        <Skeleton variant="rect" height={45} className={classes.select} />
+      )}
+
+      <List>
+        <Link href="/">
+          <ListItem button>
+            <ListItemIcon>
+              <VideoCallIcon />
+            </ListItemIcon>
+            <ListItemText>
+              <Typography variant="h3">Templates</Typography>
+            </ListItemText>
+          </ListItem>
+        </Link>
+
+        <Link href="/collaterals">
+          <ListItem button>
+            <ListItemIcon>
+              <PresentIcon />
+            </ListItemIcon>
+            <ListItemText>
+              <Typography variant="h3">Collatorals</Typography>
+            </ListItemText>
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
+
+  const createWorkspaceModal = (
+    <Dialog open={isCreatingWorkspace} onClose={() => setIsCreatingWorkspace(false)}>
+      <DialogTitle>Create New Workspace</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          A workspace allows you to collaborate with a group of co-workers on calls.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Workspace Name"
+          type="text"
+          fullWidth
+          value={newWorkspaceName}
+          onChange={(e) => setNewWorkspaceName(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setIsCreatingWorkspace(false)} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleCreateWorkspace} color="primary">
+          Create
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+
+  const container = isBrowser ? () => window.document.body : undefined;
+
+  return (
+    <nav className={classes.drawer} aria-label="mailbox folders">
+      {createWorkspaceModal}
+
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Hidden smUp implementation="css">
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor="right"
+          open={mobileOpen}
+          onClose={toggleOpen}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <Drawer
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          variant="permanent"
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+    </nav>
+  );
+}
