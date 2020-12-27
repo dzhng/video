9fa5070a3c5378023962b02f3b1b222f0fc49029
David Zhang <dzz0615@gmail.com>
2020-12-27 16:50:49 +0700

    acitvity editing

--- src/components/ActivitiesBar/ActivityCard.tsx
+++ src/components/ActivitiesBar/ActivityCard.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import { Card, Typography, IconButton } from '@material-ui/core';
+import React, { useState, useRef, useCallback, useMemo } from 'react';
+import * as Yup from 'yup';
+import { debounce } from 'lodash';
+import {
+  Card,
+  Typography,
+  InputBase,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { MoreVert as MoreIcon } from '@material-ui/icons';
+import { useAppState } from '~/state';
+
+const NameSchema = Yup.string().min(1, 'Too Short!').max(50, 'Too Long!').required();
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,8 +29,28 @@ const useStyles = makeStyles((theme: Theme) =>
         boxShadow: theme.shadows[4],
       },
     },
+    nameInput: {
+      ...theme.typography.h2,
+      width: '100%',
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+
+      // have transparent border here so its correctly sized
+      border: '1px solid transparent',
+      borderRadius: theme.shape.borderRadius,
+      transition: '0.3s',
+
+      '&:hover': {
+        border: '1px solid ' + theme.palette.grey[300],
+      },
+
+      '&.Mui-focused': {
+        border: '1px solid ' + theme.palette.grey[500],
+      },
+    },
     activityType: {
       color: theme.palette.grey[600],
+      paddingLeft: theme.spacing(1),
     },
     content: {
       flexGrow: 1,
@@ -27,19 +60,60 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function ActivitiesCard() {
   const classes = useStyles();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [name, setName] = useState('Activity name');
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const { markIsWriting } = useAppState();
+
+  const handleSettingsClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setMenuOpen((state) => !state);
+  };
+
+  const debouncedSaveName = useMemo(() => {
+    const saveName = (newName: string) => {
+      //db.collection(Collections.TEMPLATES).doc(template.id).update({ name: newName });
+      markIsWriting();
+    };
+
+    return debounce(saveName, 200, { maxWait: 2000, trailing: true });
+  }, [markIsWriting]);
+
+  const handleNameChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newName = e.target.value;
+      // if not valid, just don't set
+      if (!NameSchema.isValidSync(newName)) {
+        return;
+      }
+
+      setName(newName);
+      debouncedSaveName && debouncedSaveName(newName);
+    },
+    [debouncedSaveName],
+  );
 
   return (
     <Card className={classes.card}>
       <div className={classes.content}>
-        <Typography variant="h2">Activity name</Typography>
+        <InputBase className={classes.nameInput} value={name} onChange={handleNameChange} />
         <Typography variant="body1" className={classes.activityType}>
           presentation
         </Typography>
       </div>
-      <div>
-        <IconButton>
-          <MoreIcon />
+      <div ref={anchorRef}>
+        <IconButton onClick={handleSettingsClick}>
+          <Tooltip title="Settings" placement="bottom">
+            <MoreIcon />
+          </Tooltip>
         </IconButton>
+        <Menu
+          open={menuOpen}
+          onClose={() => setMenuOpen((state) => !state)}
+          anchorEl={anchorRef.current}
+        >
+          <MenuItem>Delete Activity</MenuItem>
+        </Menu>
       </div>
     </Card>
   );
