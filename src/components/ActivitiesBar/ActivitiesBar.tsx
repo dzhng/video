6a0adee3434ed26b7ea670066dd452657d733727
David Zhang <dzz0615@gmail.com>
2020-12-28 15:48:10 +0700

    works but looks a bit ugly

--- src/components/ActivitiesBar/ActivitiesBar.tsx
+++ src/components/ActivitiesBar/ActivitiesBar.tsx
@@ -11,6 +11,7 @@ import {
 } from '@material-ui/lab';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { AddOutlined as AddIcon } from '@material-ui/icons';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import PresentationPicker from '~/components/PresentationPicker/PresentationPicker';
 import NotesEditor from '~/components/NotesEditor/NotesEditor';
 import ActivityCard from './ActivityCard';
@@ -32,42 +33,60 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const ActivityTimelineItem = () => (
-  <TimelineItem>
-    <TimelineOppositeContent></TimelineOppositeContent>
-    <TimelineSeparator>
-      <TimelineDot />
-      <TimelineConnector />
-    </TimelineSeparator>
-    <TimelineContent>
-      <ActivityCard />
-    </TimelineContent>
-  </TimelineItem>
+const ActivityTimelineItem = ({ id, index }: { id: string; index: number }) => (
+  <Draggable draggableId={id} index={index}>
+    {({ innerRef, draggableProps, dragHandleProps }) => (
+      <TimelineItem ref={innerRef} {...draggableProps} {...dragHandleProps}>
+        <TimelineOppositeContent></TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <ActivityCard />
+        </TimelineContent>
+      </TimelineItem>
+    )}
+  </Draggable>
 );
 
 export default function ActivitiesBar() {
   const classes = useStyles();
 
-  return (
-    <div className={classes.container}>
-      <Timeline>
-        <ActivityTimelineItem />
-        <ActivityTimelineItem />
-        <ActivityTimelineItem />
-        <ActivityTimelineItem />
+  const onDragEnd = useCallback(() => {}, []);
 
-        <TimelineItem>
-          <TimelineOppositeContent></TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Button variant="contained" color="secondary" fullWidth className={classes.addButton}>
-              <AddIcon /> New Activity
-            </Button>
-          </TimelineContent>
-        </TimelineItem>
-      </Timeline>
-    </div>
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list">
+        {({ innerRef, droppableProps, placeholder }) => (
+          <div className={classes.container} ref={innerRef} {...droppableProps}>
+            <Timeline>
+              <ActivityTimelineItem id="1" index={1} />
+              <ActivityTimelineItem id="2" index={2} />
+              <ActivityTimelineItem id="3" index={3} />
+              <ActivityTimelineItem id="4" index={4} />
+              {placeholder}
+
+              <TimelineItem>
+                <TimelineOppositeContent></TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    fullWidth
+                    className={classes.addButton}
+                  >
+                    <AddIcon /> New Activity
+                  </Button>
+                </TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
