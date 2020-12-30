6beb7dedbae97e9f6e5b76762ba1c63a8c309451
David Zhang <dzz0615@gmail.com>
2020-12-30 20:31:30 +0700

    seperated out switch control

--- src/components/ActivitiesBar/Types/PollActivity.tsx
+++ src/components/ActivitiesBar/Types/PollActivity.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Switch, TextField } from 'formik-material-ui';
-import { Field, FieldArray, ErrorMessage, useFormikContext } from 'formik';
+import { TextField } from 'formik-material-ui';
+import { Field, FieldArray, useFormikContext } from 'formik';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import {
-  InputAdornment,
-  IconButton,
-  Tooltip,
-  Button,
-  Divider,
-  Typography,
-} from '@material-ui/core';
+import { InputAdornment, IconButton, Tooltip, Button, Divider } from '@material-ui/core';
 import { ClearOutlined as DeleteIcon, AddOutlined as AddIcon } from '@material-ui/icons';
 import { PollActivityMetadata } from '~/firebase/schema-types';
+import SwitchControl from '~/components/Form/SwitchControl';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,31 +15,12 @@ const useStyles = makeStyles((theme: Theme) =>
     divider: {
       margin: theme.spacing(1),
     },
-    switchControl: {
-      display: 'flex',
-      alignItems: 'center',
-      color: theme.palette.grey[800],
-      borderRadius: theme.shape.borderRadius,
-
-      '& .MuiTypography-root': {
-        flexGrow: 1,
-        marginLeft: theme.spacing(1),
-      },
-
-      '&:hover': {
-        backgroundColor: theme.palette.grey[100],
-      },
-    },
   }),
 );
 
 export default function CreatePollActivity() {
   const classes = useStyles();
   const { values } = useFormikContext<PollActivityMetadata>();
-
-  const MyErrorMessage = ({ name }: { name: string }) => (
-    <ErrorMessage className={classes.error} name={name} component="span" />
-  );
 
   return (
     <div>
@@ -85,17 +60,8 @@ export default function CreatePollActivity() {
 
       <Divider className={classes.divider} />
 
-      <div className={classes.switchControl}>
-        <Typography variant="body1">Show results right away</Typography>
-        <Field component={Switch} type="checkbox" name="showResultsRightAway" />
-        <MyErrorMessage name="showResultsRightAway" />
-      </div>
-
-      <div className={classes.switchControl}>
-        <Typography variant="body1">Multiple choice</Typography>
-        <Field component={Switch} type="checkbox" name="isMultipleChoice" />
-        <MyErrorMessage name="isMultipleChoice" />
-      </div>
+      <SwitchControl name="showResultsRightAway" title="Show results right away" />
+      <SwitchControl name="isMultipleChoice" title="Multiple choice" />
     </div>
   );
 }
---
+++ src/components/Form/SwitchControl.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Field, ErrorMessage } from 'formik';
+import { Switch } from 'formik-material-ui';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    error: {
+      color: theme.palette.error.main,
+    },
+    switchControl: {
+      display: 'flex',
+      alignItems: 'center',
+      color: theme.palette.grey[800],
+      borderRadius: theme.shape.borderRadius,
+
+      '& .MuiTypography-root': {
+        flexGrow: 1,
+        marginLeft: theme.spacing(1),
+      },
+
+      '&:hover': {
+        backgroundColor: theme.palette.grey[100],
+      },
+    },
+  }),
+);
+
+export default function ({ name, title }: { name: string; title: string }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.switchControl}>
+      <Typography variant="body1">{title}</Typography>
+      <Field component={Switch} type="checkbox" name={name} />
+      <ErrorMessage className={classes.error} name={name} component="span" />
+    </div>
+  );
+}
