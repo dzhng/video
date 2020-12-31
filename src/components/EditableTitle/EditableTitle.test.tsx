c44b7287221ffb9d52adfae8827e7baeaa29f968
David Zhang <dzz0615@gmail.com>
2020-12-31 14:03:02 +0700

    added empty tests

---
+++ src/components/EditableTitle/EditableTitle.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+
+// TODO:
+describe('EditableTitle', () => {
+  it('should show loading skeleton when isLoading is set to true', () => {});
+
+  it('should not call change callback when isLoading is set to true', () => {});
+
+  it('should display input and submit when isLoading is set to false', () => {});
+
+  it('should submit right away when debounceChange is set to false', () => {});
+
+  it('should submit every 2 seconds by default', () => {});
+});
