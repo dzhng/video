e5cb15e8e6cfbcc1709e5758e293aa6f9bc9c1cb
David Zhang <dzz0615@gmail.com>
2021-01-05 12:36:55 +0700

    styled activity display wrapper

--- src/components/Activities/CallDisplay/ActivityDisplay.tsx
+++ src/components/Activities/CallDisplay/ActivityDisplay.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Typography, Card } from '@material-ui/core';
 import { ActivityTypeConfig } from '../Types/Types';
 import useCallContext from '~/hooks/useCallContext/useCallContext';
 import ErrorDisplay from './Error';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    header: {
+      padding: theme.spacing(1),
+      backgroundColor: theme.palette.grey[300],
+    },
+    content: {
+      flexGrow: 1,
+    },
+  }),
+);
+
 export default function ActivityDisplay() {
+  const classes = useStyles();
   const { call, template } = useCallContext();
 
   if (!call || !call.currentActivityId) {
@@ -20,5 +41,12 @@ export default function ActivityDisplay() {
     return <ErrorDisplay />;
   }
 
-  return config.display;
+  return (
+    <Card className={classes.container}>
+      <div className={classes.header}>
+        <Typography variant="h2">{activity.name}</Typography>
+      </div>
+      <div className={classes.content}>{config.display}</div>
+    </Card>
+  );
 }
--- src/components/Activities/CallDisplay/Presentation.tsx
+++ src/components/Activities/CallDisplay/Presentation.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import useCallContext from '~/hooks/useCallContext/useCallContext';
 
 export default function PresentationDisplay() {
+  const { updateActivity } = useCallContext();
+
   return <div>Presentation Display</div>;
 }
