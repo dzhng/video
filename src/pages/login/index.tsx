7e134266753ccda2d646bfe8849db80f866b2b75
David Zhang <dzz0615@gmail.com>
2021-01-10 19:04:31 +0700

    pretty login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,46 +1,47 @@
 import React, { useState, useCallback } from 'react';
 import { useRouter } from 'next/router';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { ErrorOutline as ErrorIcon } from '@material-ui/icons';
+import { Typography, Button, Paper, Container } from '@material-ui/core';
 
 import { useAppState } from '~/state';
 import LoadingContainer from '~/containers/Loading/Loading';
 
-const useStyles = makeStyles({
-  container: {
-    height: '100vh',
-    background: '#0D122B',
-  },
-  paper: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-    maxWidth: '460px',
-    padding: '2em',
-    marginTop: '4em',
-    background: 'white',
-    color: 'black',
-  },
-  button: {
-    color: 'black',
-    background: 'white',
-    margin: '0.8em 0 0.7em',
-    textTransform: 'none',
-  },
-  errorMessage: {
-    color: 'red',
-    display: 'flex',
-    alignItems: 'center',
-    margin: '1em 0 0.2em',
-    '& svg': {
-      marginRight: '0.4em',
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    container: {
+      height: '100vh',
     },
-  },
-});
+    paper: {
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      padding: '2em',
+      marginTop: 'calc(50vh - 150px)',
+
+      '& h1': {
+        marginBottom: theme.spacing(1),
+      },
+      '& p': {
+        marginBottom: theme.spacing(1),
+      },
+    },
+    button: {
+      color: 'black',
+      background: 'white',
+      margin: '0.8em 0 0.7em',
+      textTransform: 'none',
+    },
+    errorMessage: {
+      color: 'red',
+      display: 'flex',
+      alignItems: 'center',
+      '& svg': {
+        marginRight: '0.4em',
+      },
+    },
+  }),
+);
 
 const GoogleLogo = <img src="/google-logo.svg" />;
 
@@ -64,23 +65,22 @@ export default function LoginPage({ previousPage }: { previousPage?: string }) {
   }
 
   return (
-    <Grid
-      container
-      justify="center"
-      alignItems="flex-start"
-      className={classes.container}
-      data-testid="container"
-    >
-      <Paper className={classes.paper} elevation={6}>
+    <Container maxWidth="xs" className={classes.container} data-testid="container">
+      <Paper className={classes.paper} elevation={3}>
+        <Typography variant="h1">Welcome to Aomni</Typography>
+        <Typography variant="body1">Sign in to get started.</Typography>
+
         <div>
           {authError && (
             <Typography variant="caption" className={classes.errorMessage}>
-              <ErrorOutlineIcon />
+              <ErrorIcon />
               {authError.message}
             </Typography>
           )}
         </div>
+
         <Button
+          fullWidth
           variant="contained"
           className={classes.button}
           onClick={login}
@@ -89,6 +89,6 @@ export default function LoginPage({ previousPage }: { previousPage?: string }) {
           Sign in with Google
         </Button>
       </Paper>
-    </Grid>
+    </Container>
   );
 }
