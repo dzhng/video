f2c5068ffdfa9e16eed3408e3e3da160b33dd735
David Zhang <dzz0615@gmail.com>
2021-01-10 18:39:29 +0700

    host end call confirm modal

--- src/components/Video/Controls/EndCallButton/EndCallButton.tsx
+++ src/components/Video/Controls/EndCallButton/EndCallButton.tsx
@@ -2,7 +2,16 @@ import React, { useCallback, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import CallEnd from '@material-ui/icons/CallEnd';
-import { Fab, Tooltip } from '@material-ui/core';
+import {
+  Fab,
+  Tooltip,
+  Dialog,
+  DialogTitle,
+  DialogContentText,
+  DialogContent,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
 
 import useVideoContext from '~/hooks/Video/useVideoContext/useVideoContext';
 import useCallContext from '~/hooks/useCallContext/useCallContext';
@@ -20,29 +29,58 @@ export default function EndCallButton(props: object) {
   const { room } = useVideoContext();
   const { isHost, endCall } = useCallContext();
   const [isEndingCall, setIsEndingCall] = useState(false);
+  const [confirmOpen, setConfirmOpen] = useState(false);
 
-  const handleEndCall = useCallback(async () => {
-    setIsEndingCall(true);
+  const handleEndCall = useCallback(() => {
     if (isHost) {
-      await endCall().catch(() => setIsEndingCall(false));
-      room.disconnect();
+      setConfirmOpen(true);
     } else {
       room.disconnect();
     }
-  }, [isHost, endCall, room]);
+  }, [isHost, room]);
+
+  const handleConfirm = useCallback(async () => {
+    setIsEndingCall(true);
+    await endCall().catch(() => setIsEndingCall(false));
+    room.disconnect();
+  }, [endCall, room]);
 
   return (
-    <Tooltip
-      title={'End Call'}
-      onClick={handleEndCall}
-      placement="top"
-      PopperProps={{ disablePortal: true }}
-    >
-      <div>
-        <Fab className={classes.fab} color="secondary" disabled={isEndingCall} {...props}>
-          <CallEnd />
-        </Fab>
-      </div>
-    </Tooltip>
+    <>
+      <Tooltip
+        title={'End Call'}
+        onClick={handleEndCall}
+        placement="top"
+        PopperProps={{ disablePortal: true }}
+      >
+        <div>
+          <Fab className={classes.fab} color="secondary" disabled={isEndingCall} {...props}>
+            <CallEnd />
+          </Fab>
+        </div>
+      </Tooltip>
+
+      <Dialog
+        open={confirmOpen}
+        onClose={() => setConfirmOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle>End Call?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            As the host, leaving the call will end the call for everyone.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setConfirmOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm} color="primary" variant="contained" autoFocus>
+            End Call
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
