b1360f805082b082aa3b92762a28810ea75f0fbc
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-01-11 22:28:46 +0400

    write partial sign in test

--- cypress/integration/signin.spec.ts
+++ cypress/integration/signin.spec.ts
@@ -1,3 +1,10 @@
 describe('sign in test', () => {
-  it('will sign in with google', () => {});
+  it('will check if page is redirect to login screen if user is not signed in', () => {
+    cy.visit('/');
+    cy.url().should('equal', Cypress.config().baseUrl + '/login');
+  });
+  it('will sign in to google and redirect to home if successful', () => {
+    cy.visit('/login');
+    cy.get('#loginButton').click();
+  });
 });
