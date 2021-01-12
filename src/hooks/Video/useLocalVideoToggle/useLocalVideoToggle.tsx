ada2f9257d93e54974f1a5ba87b49acdb36e177b
David Zhang <dzz0615@gmail.com>
2021-01-12 16:30:58 +0700

    simpler toggle local video button, make use of track stop / restart instead of creating new tracks

--- src/hooks/Video/useLocalVideoToggle/useLocalVideoToggle.tsx
+++ src/hooks/Video/useLocalVideoToggle/useLocalVideoToggle.tsx
@@ -1,47 +1,37 @@
 import { LocalVideoTrack } from 'twilio-video';
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 import useVideoContext from '../useVideoContext/useVideoContext';
 
 export default function useLocalVideoToggle() {
   const {
     room: { localParticipant },
     localTracks,
-    getLocalVideoTrack,
-    removeLocalVideoTrack,
     onError,
   } = useVideoContext();
   const videoTrack = localTracks.find((track) => track.name.includes('camera')) as LocalVideoTrack;
-  const [isPublishing, setIsPublishing] = useState(false);
-  const previousDeviceIdRef = useRef<string>();
 
-  // TODO: useEffect, if localParticipant exist, publish existing videoTrack. If this isn't here, there'll be a bug where if you join a room, local participant won't publish anything unless if toggle is called
+  // tracks if currently publishing track, ignore toggle if it is still publishing
+  const [isPublishing, setIsPublishing] = useState(false);
 
   const toggleVideoEnabled = useCallback(() => {
-    if (!isPublishing) {
-      if (videoTrack) {
-        previousDeviceIdRef.current = videoTrack.mediaStreamTrack.getSettings().deviceId;
-        const localTrackPublication = localParticipant?.unpublishTrack(videoTrack);
-        // TODO: remove when SDK implements this event. See: https://issues.corp.twilio.com/browse/JSDK-2592
-        localParticipant?.emit('trackUnpublished', localTrackPublication);
-        removeLocalVideoTrack();
-      } else {
-        setIsPublishing(true);
-        getLocalVideoTrack({ deviceId: { exact: previousDeviceIdRef.current } })
-          .then((track: LocalVideoTrack) =>
-            localParticipant?.publishTrack(track, { priority: 'low' }),
-          )
-          .catch(onError)
-          .finally(() => setIsPublishing(false));
-      }
+    if (isPublishing || !videoTrack) {
+      return;
     }
-  }, [
-    videoTrack,
-    localParticipant,
-    getLocalVideoTrack,
-    isPublishing,
-    onError,
-    removeLocalVideoTrack,
-  ]);
 
-  return [!!videoTrack, toggleVideoEnabled] as const;
+    if (videoTrack.isStopped) {
+      setIsPublishing(true);
+      videoTrack.restart();
+      localParticipant
+        ?.publishTrack(videoTrack, { priority: 'low' })
+        .catch(onError)
+        .finally(() => setIsPublishing(false));
+    } else {
+      videoTrack.stop();
+      const localTrackPublication = localParticipant?.unpublishTrack(videoTrack);
+      // TODO: remove when SDK implements this event. See: https://issues.corp.twilio.com/browse/JSDK-2592
+      localParticipant?.emit('trackUnpublished', localTrackPublication);
+    }
+  }, [videoTrack, localParticipant, isPublishing, onError]);
+
+  return [!videoTrack.isStopped, toggleVideoEnabled] as const;
 }
