dc72348dfca0765fc0c0a1e04c77a958a0e77e21
David Zhang <dzz0615@gmail.com>
2021-01-12 17:19:01 +0700

    different behavior for video toggle between mobile / desktop

--- src/hooks/Video/useLocalVideoToggle/useLocalVideoToggle.tsx
+++ src/hooks/Video/useLocalVideoToggle/useLocalVideoToggle.tsx
@@ -1,6 +1,9 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useCallback, useState } from 'react';
+import { isMobile } from '~/utils';
 import useVideoContext from '../useVideoContext/useVideoContext';
 
+// video is a bit more complicated than audio, because we need to completely stop
+// video track instead of just disabling, or else the camera light will be on
 export default function useLocalVideoToggle() {
   const {
     room: { localParticipant },
@@ -8,6 +11,14 @@ export default function useLocalVideoToggle() {
     onError,
   } = useVideoContext();
   const videoTrack = localTracks.find((track) => track.name.includes('camera'));
+  const [isEnabled, setIsEnabled] = useState<boolean>(false);
+
+  // everytime video track changes, set a new enabled flag
+  useEffect(() => {
+    if (videoTrack) {
+      setIsEnabled(true);
+    }
+  }, [videoTrack]);
 
   // tracks if currently publishing track, ignore toggle if it is still publishing
   const [isPublishing, setIsPublishing] = useState(false);
@@ -17,20 +28,29 @@ export default function useLocalVideoToggle() {
       return;
     }
 
-    if (videoTrack.isStopped) {
-      setIsPublishing(true);
-      videoTrack.restart();
-      localParticipant
-        ?.publishTrack(videoTrack, { priority: 'low' })
-        .catch(onError)
-        .finally(() => setIsPublishing(false));
+    // for mobile - Android chrome has a bug where it freeze when track is stopped,
+    // so enable/disable instead. Also makes it less resource intensive.
+    if (isMobile) {
+      isEnabled ? videoTrack.disable() : videoTrack.enable();
+      setIsEnabled(!isEnabled);
     } else {
-      videoTrack.stop();
-      const localTrackPublication = localParticipant?.unpublishTrack(videoTrack);
-      // TODO: remove when SDK implements this event. See: https://issues.corp.twilio.com/browse/JSDK-2592
-      localParticipant?.emit('trackUnpublished', localTrackPublication);
+      if (isEnabled) {
+        videoTrack.stop();
+        const localTrackPublication = localParticipant?.unpublishTrack(videoTrack);
+        // TODO: remove when SDK implements this event. See: https://issues.corp.twilio.com/browse/JSDK-2592
+        localParticipant?.emit('trackUnpublished', localTrackPublication);
+        setIsEnabled(false);
+      } else {
+        setIsPublishing(true);
+        videoTrack
+          .restart()
+          .then(() => localParticipant?.publishTrack(videoTrack, { priority: 'low' }))
+          .then(() => setIsEnabled(true))
+          .catch(onError)
+          .finally(() => setIsPublishing(false));
+      }
     }
-  }, [videoTrack, localParticipant, isPublishing, onError]);
+  }, [isEnabled, videoTrack, localParticipant, isPublishing, onError]);
 
-  return [videoTrack ? !videoTrack.isStopped : false, toggleVideoEnabled] as const;
+  return [isEnabled, toggleVideoEnabled] as const;
 }
