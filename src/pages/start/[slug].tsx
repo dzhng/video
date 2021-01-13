fb767f69bd883a55ef7db24dc0d881f69688df57
David Zhang <dzz0615@gmail.com>
2021-01-13 09:19:06 +0700

    moved head tag so its clearer

--- src/pages/start/[slug].tsx
+++ src/pages/start/[slug].tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import { Collections, LocalModel, Template } from '~/firebase/schema-types';
@@ -66,9 +67,16 @@ export default function StartPage() {
 
   // when both template and host status is ready, show call conatiner
   return template && isHost !== undefined ? (
-    <CallProvider template={template} isHost={isHost}>
-      <CallContainer />
-    </CallProvider>
+    <>
+      <Head>
+        {/* On android, make browser UI dark in call mode */}
+        <meta name="theme-color" content="#222" />
+      </Head>
+
+      <CallProvider template={template} isHost={isHost}>
+        <CallContainer />
+      </CallProvider>
+    </>
   ) : (
     <LoadingContainer />
   );
