ad8ea9e1edd8091cce3629e0b9fd8bc6714c93a8
David Zhang <dzz0615@gmail.com>
2021-01-21 19:03:40 +0700

    fixed typings

--- src/components/Video/VideoTrack/VideoTrack.tsx
+++ src/components/Video/VideoTrack/VideoTrack.tsx
@@ -31,6 +31,7 @@ export default function VideoTrack({ track, isLocal, priority }: VideoTrackProps
       track.detach(el);
       if (track.setPriority && priority) {
         // Passing `null` to setPriority will set the track's priority to that which it was published with.
+        // @ts-ignore: typing for this doesn't include null
         track.setPriority(null);
       }
     };
