f78cd1c6988bff12099e06bf629e2624e82c61ff
David Zhang <dzz0615@gmail.com>
2021-01-27 13:11:20 +0700

    added reactions display in summary

--- src/containers/Summary/Chats.tsx
+++ src/containers/Summary/Chats.tsx
@@ -1,8 +1,15 @@
 import React, { useRef } from 'react';
-import { get, values } from 'lodash';
+import { get, values, entries } from 'lodash';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import { CallData, ChatsDataKey, PublicChatsChannelKey } from '~/firebase/rtdb-types';
+import { Typography, Tooltip } from '@material-ui/core';
+import {
+  CallData,
+  ChatsDataKey,
+  PublicChatsChannelKey,
+  ReactionsCountDataKey,
+  ReactionMap,
+  ReactionTypes,
+} from '~/firebase/rtdb-types';
 import { MessageList } from '~/components/Call/Chats';
 
 const useStyles = makeStyles((theme) =>
@@ -16,6 +23,22 @@ const useStyles = makeStyles((theme) =>
       borderRadius: theme.shape.borderRadius,
       border: theme.dividerBorder,
     },
+    reactionContainer: {
+      margin: theme.spacing(1),
+      marginBottom: 0,
+      borderBottom: theme.dividerBorder,
+    },
+    reaction: {
+      display: 'inline-block',
+      marginRight: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      padding: theme.spacing(1),
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: theme.palette.grey[100],
+      border: theme.dividerBorder,
+      overflow: 'hidden',
+      cursor: 'default',
+    },
   }),
 );
 
@@ -24,11 +47,28 @@ export default function Chats({ data }: { data: CallData }) {
   const scrollRef = useRef(0);
 
   const list = values(get(data, [ChatsDataKey, PublicChatsChannelKey], {}));
+  const reactions = entries(get(data, [ReactionsCountDataKey]));
 
   return (
     <>
       <Typography variant="h2">Chats</Typography>
       <div className={classes.container}>
+        {reactions.length > 0 ? (
+          <div className={classes.reactionContainer}>
+            {reactions.map(([type, count]) => (
+              <Tooltip title={`${count} reactions were sent in this call`}>
+                <span key={type} className={classes.reaction}>
+                  {ReactionMap[type as ReactionTypes]} {count}
+                </span>
+              </Tooltip>
+            ))}
+          </div>
+        ) : (
+          <Typography variant="body1" style={{ margin: 8, color: '#888' }}>
+            No reactions were sent in this call.
+          </Typography>
+        )}
+
         {list.length > 0 ? (
           <MessageList data={data} scrollRef={scrollRef} />
         ) : (
