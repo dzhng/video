81f21f7643be53243f7fd5ea048af431a84b33c5
David Zhang <dzz0615@gmail.com>
2021-01-28 17:40:12 +0700

    compile issue

--- src/components/ActivityControl/ActivityControl.tsx
+++ src/components/ActivityControl/ActivityControl.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Fab } from '@material-ui/core';
+import { Card } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import useCallContext from '~/hooks/useCallContext/useCallContext';
 import ActivitiesBar from '~/components/Activities/ActivitiesBar/ActivitiesBar';
